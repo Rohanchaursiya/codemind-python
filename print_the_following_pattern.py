cb2dc76275296738911636534862e79a4ea8c9ae
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-05-02 09:49:16 +0530

    n=int(input()) j=n-1 for i in range(1,n+1):     print(" "*j,end="")     print(10**(2*i-1)//9 *i)     j-=1

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,6 @@
 n=int(input())
+j=n-1
 for i in range(1,n+1):
-    for j in range(1,n+1):
-        if (i==j or j==1 or j==n):
-            print("* ",end="")
-        else:
-            print("  ",end="")
-    print()
+    print(" "*j,end="")
+    print(10**(2*i-1)//9 *i)
+    j-=1
