2d17daf2b55daeefc460115b5778a633ac2230f5
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2022-10-15 23:28:36 +0530

    def prime(i):     if i==1:         return 0     c=0     for j in range(2,(i//2)+1):         if i%j==0:             c+=1     if c==0:         return i
    n=int(input())
    for i in range(n,0,-1):
        if prime(i):
            l=i
            break
    for i in range(n+1,n*n):
        if prime(i):
            s=i
            break
    if (s-n)>=(n-l):
        print(n-l)
    else:
        print(s-n)

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -1,24 +1,23 @@
-def prime(n):
-    for i in range(2,n):
-        if n%i==0:
-            return False
-    else:
-        return True
+def prime(i):
+    if i==1:
+        return 0
+    c=0
+    for j in range(2,(i//2)+1):
+        if i%j==0:
+            c+=1
+    if c==0:
+        return i
+
 n=int(input())
-if n==2 or prime(n)==1:
-    print("0")
+for i in range(n,0,-1):
+    if prime(i):
+        l=i
+        break
+for i in range(n+1,n*n):
+    if prime(i):
+        s=i
+        break
+if (s-n)>=(n-l):
+    print(n-l)
 else:
-    i=n+1
-    while(1):
-        if prime(i)==1:
-            break
-        i+=1
-    t=n-1
-    while(1):
-        if prime(t)==1:
-            break
-        t-=1
-    if abs(i-n)>abs (n-t):
-        print(abs(n-t))
-    else:
-        print(abs(i-n))
+    print(s-n)
